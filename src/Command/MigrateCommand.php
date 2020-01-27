07aa322b72373a8b6f270c005f33883c75643955
Samir Boulil <samir.boulil@akeneo.com>
2020-01-27 16:14:34 +0100

    AST-256: Fix arguments order of the migrate command to be able to run it with Saas migration script

--- src/Command/MigrateCommand.php
+++ src/Command/MigrateCommand.php
@@ -71,9 +71,9 @@ class MigrateCommand extends Command
     If you only have non localizable assets, it will create and migrate an asset family with a non localizable reference and non localizable variations.
     If you only have localizable assets, it will create and migrate an asset family with localizable reference and localizable variations.
     If you have localizable and non localizable assets, it will create an asset family with both fields.")
-            ->addArgument('pim-path', InputArgument::REQUIRED, 'The path to your PIM Enterprise Edition installation')
             ->addArgument('assets-csv-filename', InputArgument::OPTIONAL, 'The path to the Assets CSV file', self::ASSETS_CSV_FILENAME)
             ->addArgument('variations-csv-filename', InputArgument::OPTIONAL, 'The path to the Variations CSV file', self::VARIATIONS_CSV_FILENAME)
+            ->addArgument('pim-path', InputArgument::OPTIONAL, 'The path to your PIM Enterprise Edition installation')
             ->addOption('asset-family-code', null, InputOption::VALUE_OPTIONAL, 'The asset family code to migrate', null)
             ->addOption('reference-type', null, InputOption::VALUE_OPTIONAL,
                 sprintf(
